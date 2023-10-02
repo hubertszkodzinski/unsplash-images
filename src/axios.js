b06a25f6fec20b071bf6468f7f34d6cd67204483
Hubert Szkodzinski <hubertszkodzinski.it@gmail.com>
2023-10-03 00:28:33 +0200

    changes with api key for requests

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const key = import.meta.env.VITE_API_KEY;
+
 export const customFetch = axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID ',
+    Authorization: `Client-ID ${key}`,
   },
 });
