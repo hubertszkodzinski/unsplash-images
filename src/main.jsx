94e7eb5347b9fbd9175a43d95bce7744e7e9c5ca
Hubert Szkodzinski <hubertszkodzinski.it@gmail.com>
2023-10-06 02:43:09 +0200

    dependencies changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 import { AppProvider } from './context';
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </AppProvider>
 );
